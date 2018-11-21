5f93253b5614a35ed14b1816b726627e12ebaa58
ana <ana_luisavs@hotmail.com>
2018-11-21 00:41:54 -0200

    oi

--- praticandoOBI/usuarios/migrations/0008_auto_20180922_1816.py
+++ praticandoOBI/usuarios/migrations/0008_auto_20180922_1816.py
@@ -6,6 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
+	('ook', '__first__'),
         ('usuarios', '0007_auto_20180922_1816'),
     ]
 
