87bf1a39d99fdd21feb96c299d9a144027595b1b
ana <ana_luisavs@hotmail.com>
2018-11-21 23:10:03 -0200

    collect static

--- praticandoOBI/obi/settings.py
+++ praticandoOBI/obi/settings.py
@@ -116,13 +116,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
 ALLOWED_HOSTS = ['*']
 
 #novo para static files
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 #static files in production
+
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
